c27bb544e01e2b42b5a913fcdfb87ff943b17a79
Isabela Crestana <crestanaisabela@gmail.com>
2025-09-16 10:53:54 -0300

    Search: fix visit order and combine into single function

--- include/search.h
+++ include/search.h
@@ -5,8 +5,7 @@
 #include "hash-table.h"
 
 void run_search(int cube[], int search_type);
-void bfs(int cubo[], QUEUE* f);
-void loopDFS(int cubo[], STACK *p);
-int iidfs(int max_depth, STACK* s, int cube[], ht_t *ht);
+NODE* exec_search(QUEUE* q, STACK* s, int *cube, int bfsSearch, int maxDepth);
+NODE* loopDFS(QUEUE *q, STACK *s, int cube[]);
 
 #endif // SEARCH_H_INCLUDED
--- src/search.c
+++ src/search.c
@@ -9,114 +9,98 @@
 void run_search(int cube[], int search_type) {
     QUEUE* q = queue_new();
     STACK* s = stack_new();
+    NODE* answer;
+
+    printf("Encontrando o caminho...\n\n");
 
     if(search_type == 2) {  // BFS
-        bfs(cube, q);
+        answer = exec_search(q, s, cube, 1, 0);
         printf("\n\nFim da busca bfs!\n\n");
-        if(!q->front)
-            printf("\nNao foi encontrado um caminho. Cubo invalido.\n");
-        else
-            show_search_result(q->front, search_type);
         queue_free(q);
     } else {  // DFS
-        loopDFS(cube, s);
+        answer = loopDFS(q, s, cube);
         printf("\n\nFim da busca dfs iterativa!\n\n");
-        if(!s->top)
-            printf("\nNao foi encontrado um caminho. Cubo invalido.\n");
-        else
-            show_search_result(s->top, search_type);
         stack_free(s);
+    }
+
+    if(!answer)
+        printf("\nNao foi encontrado um caminho. Cubo invalido.\n");
+    else
+    {
+        show_search_result(answer, search_type, cube);
+        free(answer);
     }
 }
 
-void bfs(int cube[], QUEUE* q)
+NODE* exec_search(QUEUE* q, STACK* s, int *cube, int bfsSearch, int maxDepth)
 {
-    int is_solved = 0;
-    int movs[15];
-    int depth;
+    int is_solved = 0, movs[15];
     ht_t *ht = ht_create();
     NODE *removed;
 
     // Inserindo nó inicial
-    enqueue(q, 0, 0, movs, cube);
+    if(bfsSearch)
+        enqueue(q, 0, 0, movs, cube);
+    else
+        push(s, 0, 0, movs, cube);
+
     ht_set(ht, cube, 0);
 
-    printf("Encontrando o caminho...\n\n");
-    do
+    while(!is_solved)
     {
-        if (!q || !q->front)
-            break;
-
-        cp(q->front->cube, cube);
-        // Visita estado do primeiro nó da fila
-        is_solved = visit_state(q->front);
-
-        if (!is_solved)
-        {
-            // Atualiza vetor de movimentos
-            cp_movs(q->front, movs);
-            depth =  q->front->depth + 1;
-
-            // Remove o nó atual da fila
-            dequeue(q);
-            // Gera sucessores usando o cubo atual
-            if(depth < 15)
-                generate_successors(q, depth, movs, cube, ht);
-        }
-    } while (!is_solved);
-}
-
-void loopDFS(int cube[], STACK *s)
-{
-    int depth = -1;
-    int search_cube[24];
-    ht_t *ht = ht_create();
-
-    printf("Encontrando o caminho...\n\n");
-
-    do
-    {
-        ht = ht_create();
-        cp(cube, search_cube);
-    }while(!iidfs(depth++, s, search_cube, ht) && depth < 15);
-
-    cp(search_cube, cube);
-}
-
-int iidfs(int max_depth, STACK* s, int cube[], ht_t *ht)
-{
-    int depth, movs[15], is_solved = 0;
-    numEstados = 0;
-    push(s, 0, 0, movs, cube);
-
-    do
-    {
-        cp(s->top->cube, cube);
-
-        // Visita estado do primeiro nó da pilha
-        if(s->top->depth == max_depth)
-            is_solved = visit_state(s->top);
-
-        if(is_solved)
-            return 1;
-
-        depth = s->top->depth;
-
-        // Atualiza vetor de movimentos
-        cp_movs(s->top, movs);
-
-        pop(s);
-
-        // Gera sucessores usando o cubo atual
-        if(depth < max_depth)
-            stack_generate_successors(s, depth+1, movs, cube, ht);
-
-        if(!s->top)
+        if (!q->front && !s->top)
         {
             ht_free(ht);
-            return 0;
+            return NULL;
         }
-    } while (!is_solved);
 
-    return 1;
+        // removendo o nó inicial da fila/pilha
+        if(bfsSearch)
+            removed = dequeue(q);
+        else
+            removed = pop(s);
+
+        // visitando o nó removido
+        is_solved = visit_state(removed);
+        // caso nao seja a resposta, gera seus sucessores
+        if (!is_solved)
+        {
+            // Gera sucessores usando o cubo atual
+            if(bfsSearch)
+                generate_successors(q, removed->depth + 1, removed->movs, removed->cube, ht);
+
+            else
+            {
+                if(removed->depth < maxDepth)
+                    stack_generate_successors(s, removed->depth + 1, removed->movs, removed->cube, ht);
+            }
+
+            // desaloca o nó removido
+            free(removed);
+        }
+        // caso esteja resolvido, retorna esse nó
+        else 
+        {
+            ht_free(ht);
+            return removed;
+        }
+    }
+    return NULL;
+}
+
+NODE* loopDFS(QUEUE *q, STACK *s, int cube[])
+{
+    int depth = -1;
+    ht_t *ht;
+    NODE* answer;
+    
+    while(depth < 15)
+    {
+        ht = ht_create();
+        answer = exec_search(q, s, cube, 0, depth++);
+        if(answer)
+            return answer;
+    }
+    
+    return NULL;
 }
